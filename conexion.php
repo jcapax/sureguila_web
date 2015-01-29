d31206e73a3ba522e606b5746afc06a8f935ff2f
juan carlos <jcapax@gmail.com>
2015-01-29 12:56:11 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 	function conexion()
 	{
 
-		if (!($link = mysql_connect("192.168.1.201","root","mysqlroot"))) 
+		if (!($link = mysql_connect("192.168.1.201","root",""))) 
   		 { 
      		echo "Error conectando a la base de datos."; 
 	      	exit(); 
